4b6f3b10dd45b97bef62c9143fcb90906ac3de5d
Raffaella Nasca <r.nasca2@studenti.uniba.it>
2021-06-12 16:30:01 +0200

    Creazione interfaccia grafica iniziale

--- src/main/java/di/uniba/map/b/adventure/type/SocketClient.java
+++ src/main/java/di/uniba/map/b/adventure/type/SocketClient.java
@@ -5,6 +5,7 @@
  */
 package di.uniba.map.b.adventure.type;
 
+import di.uniba.map.b.adventure.interfacee.Inizio;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -29,29 +31,38 @@ public class SocketClient {
         InetAddress add = InetAddress.getByName("localhost");
         System.out.println(add);//localhost/127.0.0.1
         Socket s = new Socket(add, 6666);
-        System.out.println("Creazione socket riuscita");
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         //ho creato uno spazio comune Flush di input
         PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(s.getOutputStream())), true);
+        Inizio interfacciaInizio = new Inizio(out);
+        interfacciaInizio.setVisible(true);
+        JOptionPane.showMessageDialog(null, "Connessione avvenuta con successo", "Connessione socket", JOptionPane.INFORMATION_MESSAGE);
+
         //spazio comune di output client-server
         //leggo dal server
         str = in.readLine();
         
         String ris_serv, ris_cl;//risposta server,risposta client
         while (true) {
-            System.out.println(str + "-");//stampa delle possibilità
+            //JOptionPane.showMessageDialog(null, str, "Sono qui", JOptionPane.INFORMATION_MESSAGE);
+            //System.out.println(str + "-");//stampa delle possibilità
             //rispondo al server
-            ris_cl = scan.next();
-            out.println(ris_cl);
+            //ris_cl = scan.next();
+            //out.println(ris_cl);
             //leggo esito
             ris_serv = in.readLine();
             if (ris_serv.equals("Adios!")) {
                 break;
             } else {
-                System.out.println(ris_serv);
+                //System.out.println(ris_serv);
+                JOptionPane.showMessageDialog(null, ris_serv, "Sono qui", JOptionPane.INFORMATION_MESSAGE);
             }
 
         }
         s.close();
+    }
+
+    public String risposta(String Risposta) {
+        return Risposta;
     }
 }
