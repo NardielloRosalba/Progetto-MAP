4dd18dcfde44098a7b0d1c1f68b21bb03fe6699e
Raffaella Nasca <r.nasca2@studenti.uniba.it>
2021-06-16 14:14:48 +0200

    Aggiunta immagine "logo" + menu swing iniziale

--- src/main/java/di/uniba/map/b/adventure/interfacee/Inizio.java
+++ src/main/java/di/uniba/map/b/adventure/interfacee/Inizio.java
@@ -41,8 +41,11 @@ public class Inizio extends javax.swing.JFrame {
         jButtonEsci = new javax.swing.JButton();
         jButtonCaricaPartita = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenuAboutUs = new javax.swing.JMenu();
+        jMenuAbout = new javax.swing.JMenu();
+        jMenuItemAboutUs = new javax.swing.JMenuItem();
+        jMenuItemAboutGame = new javax.swing.JMenuItem();
         jMenuHelp = new javax.swing.JMenu();
+        jMenuItemHelp = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("PianetaGame");
@@ -88,12 +91,28 @@ public class Inizio extends javax.swing.JFrame {
 
         jMenuBar1.setBackground(new java.awt.Color(0, 0, 0));
 
-        jMenuAboutUs.setForeground(new java.awt.Color(51, 51, 255));
-        jMenuAboutUs.setText("About Us");
-        jMenuBar1.add(jMenuAboutUs);
+        jMenuAbout.setForeground(new java.awt.Color(51, 51, 255));
+        jMenuAbout.setText("About");
+
+        jMenuItemAboutUs.setText("About Us");
+        jMenuItemAboutUs.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItemAboutUsActionPerformed(evt);
+            }
+        });
+        jMenuAbout.add(jMenuItemAboutUs);
+
+        jMenuItemAboutGame.setText("About Game");
+        jMenuAbout.add(jMenuItemAboutGame);
+
+        jMenuBar1.add(jMenuAbout);
 
         jMenuHelp.setForeground(new java.awt.Color(51, 51, 255));
         jMenuHelp.setText("Help");
+
+        jMenuItemHelp.setText("Help comandi");
+        jMenuHelp.add(jMenuItemHelp);
+
         jMenuBar1.add(jMenuHelp);
 
         setJMenuBar(jMenuBar1);
@@ -150,6 +169,11 @@ public class Inizio extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_jButtonCaricaPartitaActionPerformed
 
+    private void jMenuItemAboutUsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemAboutUsActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jMenuItemAboutUsActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -194,8 +218,11 @@ public class Inizio extends javax.swing.JFrame {
     private javax.swing.JButton jButtonEsci;
     private javax.swing.JButton jButtonIniziaPartita1;
     private javax.swing.JLabel jLabelImage;
-    private javax.swing.JMenu jMenuAboutUs;
+    private javax.swing.JMenu jMenuAbout;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenu jMenuHelp;
+    private javax.swing.JMenuItem jMenuItemAboutGame;
+    private javax.swing.JMenuItem jMenuItemAboutUs;
+    private javax.swing.JMenuItem jMenuItemHelp;
     // End of variables declaration//GEN-END:variables
 }
